a782b83c94363c5f905330d845c45037ee03db17
dgjPolyTech <dgjpolytech@gmail.com>
2025-08-26 19:35:07 +0900

    주석으로 설명 추가

--- ch06_chatbot/cb.py
+++ ch06_chatbot/cb.py
@@ -7,8 +7,29 @@ def main():
     st.set_page_config(layout = "wide") # 구조를 wide(양 화면 꽉 차게)로 구성. 외에는 centered 옵션도 있음.
     st.title("친근한 AI 챗봇(셀프)") # 페이지 상단 제목 추가
     st.caption("스트림릿과 OpenAI API를 활용한 간단한 챗봇") # 제목 아래에 작은 글씨로 제목 생성
+
+     # 아래처럼, markdown 언어로 스타일 지정하면 스트림릿 관련 css 설정도 가능함.
+    # st.markdown(
+    #     """
+    #     <style>
+    #     body {
+    #         background-color: #f0f2f6; /* 연한 회색 배경 */
+    #     }
+    #     .stApp {
+    #         background-color: #f0f2f6; /* Streamlit 앱 전체의 배경색 */
+    #     }
+    #     /* 추가적인 CSS 스타일을 여기에 작성할 수 있습니다. */
+    #     /* 예: 사이드바 배경색 변경 */
+    #     .css-vk325g.e1fqkh3o1 { /* Streamlit 사이드바 컴포넌트 클래스 */
+    #         background-color: #e0e2e6;
+    #     }
+    #     </style>
+    #     """,
+    #     unsafe_allow_html=True # HTML 태그를 허용해야 CSS가 적용됩니다.
+    # )
     
     # 3. sidebar 추가(이미 만들어진 것 사용)(메인과 별개임)
+    # with ~: ~안에 요소를 렌더링 시킬 때 사용함.
     with st.sidebar:
         st.subheader("OpenAI API Key 설정") # 사이드바 제목
         openai_api_key = st.text_input("OpenAI API Key", type="password") # apiKey를 password 형태로 입력 받아, openai_api_key라는 변수에 저장.
@@ -26,13 +47,17 @@ def main():
     """
 
     # 세션에 이전 메시지에 대한 내용이 없으면, 프롬프트를 토대로 답변
+    # 세션: 프로그램이 한번 실행되는 동안 생기는 일종의 임시 저장공간.
     if "messages" not in st.session_state:
         st.session_state.messages = [{"role": 'system', "content": system_message}]
 
+    # 반복문 초기 시작값
     idx = 0
 
     for message in st.session_state.messages:
+        # 이전 메시지가 있으면(idx > 0), 화면 상에 다시 그려주는 부분
         if idx > 0:
+
             with st.chat_message(message["role"]):
                 st.write(message["content"])
         idx = idx + 1
@@ -43,7 +68,7 @@ def main():
 
     user_input = st.chat_input("무엇이 궁금하신가요?") # 채팅창 생성. () 안은 채팅창 안에 입력될 문구
 
-    # 사용자가 입력하면
+    # 사용자가 입력하면, 아래의 동작이 실행됨.
     if user_input:
         st.session_state.messages.append({"role": "user", "content": user_input})
         # 사용자의 채팅 메시지를 표시
